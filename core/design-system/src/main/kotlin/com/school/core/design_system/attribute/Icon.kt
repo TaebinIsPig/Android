bdf1acf11243397e4f8c8fb97ae91bdc2e00e1e5
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-11-27 08:24:06 +0900

    비밀번호 아이콘 추가

--- core/design-system/src/main/kotlin/com/school/core/design_system/attribute/Icon.kt
+++ core/design-system/src/main/kotlin/com/school/core/design_system/attribute/Icon.kt
@@ -3,12 +3,14 @@ package com.school.core.design_system.attribute
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.DefaultAlpha
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import com.school.core.design_system.R
 import javax.annotation.concurrent.Immutable
 
 @Composable
@@ -37,6 +39,15 @@ class SchoolIconList private constructor(
     val contentDescription: String? = null,
 ) {
     companion object {
-
+        @Stable
+        val PasswordVisible = SchoolIconList(
+            drawableId = R.drawable.ic_password_visible,
+            contentDescription = "passwordVisibleIcon"
+        )
+        @Stable
+        val PasswordHide = SchoolIconList(
+            drawableId = R.drawable.ic_password_hide,
+            contentDescription = "passwordHideIcon"
+        )
     }
 }
