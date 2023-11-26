6118d5070b2121977b94a555b83638848e1a6814
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-11-26 21:18:27 +0900

    gitignore 적용

--- buildSrc/build/kotlin-dsl/plugins-blocks/extracted/school-core.gradle.kts
+++
@@ -1,9 +0,0 @@
-                                                                             
-                                       
-
-plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
-    id("dagger.hilt.android.plugin")
-    id("com.google.devtools.ksp")
-}
--- buildSrc/build/kotlin-dsl/plugins-blocks/extracted/school-feature.gradle.kts
+++
@@ -1,9 +0,0 @@
-                                                                             
-                                       
-
-plugins {
-    id("com.android.library")
-    id("kotlin-android")
-    id("dagger.hilt.android.plugin")
-    id("com.google.devtools.ksp")
-}
