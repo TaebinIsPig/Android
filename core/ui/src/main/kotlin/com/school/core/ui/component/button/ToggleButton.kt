ba0eb2da9a017d2d84d2d2931e06b54aa1c77ceb
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-11-27 09:52:42 +0900

    toggleButton removeOverScroll을 이용하여 간소화

--- core/ui/src/main/kotlin/com/school/core/ui/component/button/ToggleButton.kt
+++ core/ui/src/main/kotlin/com/school/core/ui/component/button/ToggleButton.kt
@@ -1,32 +1,36 @@
 package com.school.core.ui.component.button
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.school.core.design_system.SchoolTheme
+import com.school.core.ui.component.list.RemoveOverScrollLazyRow
 import com.school.core.ui.component.textview.BodyLargeText
 import com.school.core.ui.util.modifier.schoolClickable
 
-enum class ToggleMenu {
-    Cafeteria,
-    TimeTable
+enum class ToggleMenu(val type: String) {
+    Cafeteria(type = "급식"),
+    TimeTable(type = "시간표")
 }
 
 @Composable
 fun ToggleButton(
     modifier: Modifier = Modifier,
     currentToggle: ToggleMenu,
-    clickToggle: () -> Unit,
+    onClickToggle: (ToggleMenu) -> Unit,
 ) {
-    Row(
+    RemoveOverScrollLazyRow(
         modifier = modifier
             .background(
                 color = SchoolTheme.colors.pink2,
@@ -34,37 +38,29 @@ fun ToggleButton(
             )
             .padding(vertical = 2.dp, horizontal = 1.dp)
     ) {
-        BodyLargeText(
-            modifier = Modifier
-                .background(
-                    color = if (currentToggle == ToggleMenu.Cafeteria) SchoolTheme.colors.main else Color.Transparent,
-                    shape = RoundedCornerShape(100.dp)
-                )
-                .padding(vertical = 9.dp, horizontal = 30.dp)
-                .schoolClickable(onClick = clickToggle),
-            text = "급식",
-            fontWeight = FontWeight.SemiBold
-        )
-        BodyLargeText(
-            modifier = Modifier
-                .background(
-                    color = if (currentToggle == ToggleMenu.TimeTable) SchoolTheme.colors.main else Color.Transparent,
-                    shape = RoundedCornerShape(100.dp)
-                )
-                .padding(vertical = 9.dp, horizontal = 23.dp)
-                .schoolClickable(onClick = clickToggle),
-            text = "시간표",
-            fontWeight = FontWeight.SemiBold
-        )
+        items(ToggleMenu.values().toList()) {
+            BodyLargeText(
+                modifier = Modifier
+                    .background(
+                        color = if (currentToggle == it) SchoolTheme.colors.main else Color.Transparent,
+                        shape = RoundedCornerShape(100.dp)
+                    )
+                    .padding(vertical = 9.dp, horizontal = 30.dp)
+                    .schoolClickable(onClick = { onClickToggle(it) }),
+                text = it.type,
+                fontWeight = FontWeight.SemiBold
+            )
+        }
     }
 }
 
 @Preview
 @Composable
 fun previewToggleButton() {
+    var currentMenu by remember { mutableStateOf(ToggleMenu.Cafeteria) }
     SchoolTheme {
-        ToggleButton(currentToggle = ToggleMenu.Cafeteria) {
-
+        ToggleButton(currentToggle = currentMenu) {
+            currentMenu = it
         }
     }
 }
