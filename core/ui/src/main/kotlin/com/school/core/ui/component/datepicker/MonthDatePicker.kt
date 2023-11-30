b9716d08620a89470ea35d40d96ae6f0fec240fc
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-11-30 11:00:17 +0900

    좌우 스크롤 기능 추가

--- core/ui/src/main/kotlin/com/school/core/ui/component/datepicker/MonthDatePicker.kt
+++ core/ui/src/main/kotlin/com/school/core/ui/component/datepicker/MonthDatePicker.kt
@@ -3,7 +3,6 @@ package com.school.core.ui.component.datepicker
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
@@ -17,28 +16,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.school.core.design_system.SchoolTheme
-import com.school.core.design_system.attribute.SchoolIcon
-import com.school.core.design_system.attribute.SchoolIconList
 import com.school.core.ui.component.textview.BodyMediumText
 import com.school.core.ui.util.data.dayOfWeekList
 import com.school.core.ui.util.data.getSpaceCount
 import com.school.core.ui.util.data.getStartOrLastDate
 import com.school.core.ui.util.data.setDate
 import com.school.core.ui.util.modifier.schoolClickable
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
 import java.time.LocalDate
 
 @OptIn(ExperimentalFoundationApi::class)
