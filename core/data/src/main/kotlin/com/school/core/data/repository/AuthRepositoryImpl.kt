562d5da5d6f9598f1055219c9ab773474fac76cc
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-12-01 13:47:41 +0900

    로그인 성공 시 토큰 정보 저장 기능 구현

--- core/data/src/main/kotlin/com/school/core/data/repository/AuthRepositoryImpl.kt
+++ core/data/src/main/kotlin/com/school/core/data/repository/AuthRepositoryImpl.kt
@@ -1,13 +1,16 @@
 package com.school.core.data.repository
 
+import com.school.core.data.local.LocalAuthDataSource
 import com.school.core.data.remote.datasource.RemoteAuthDateSource
 import com.school.core.data.remote.request.auth.toRequest
+import com.school.core.data.remote.response.auth.TokenResponse
 import com.school.core.domain.param.SignInParam
 import com.school.core.domain.param.SignupParam
 import com.school.core.domain.repository.AuthRepository
 import javax.inject.Inject
 
 class AuthRepositoryImpl @Inject constructor(
+    private val localAuthDataSource: LocalAuthDataSource,
     private val remoteAuthDateSource: RemoteAuthDateSource,
 ) : AuthRepository {
     override suspend fun signup(signupParam: SignupParam) =
@@ -19,8 +22,14 @@ class AuthRepositoryImpl @Inject constructor(
     override suspend fun verifyCertificate(authCode: Int, phoneNumber: String) =
         remoteAuthDateSource.verifyCertificate(authCode = authCode, phoneNumber = phoneNumber)
 
-    override suspend fun signIn(signInParam: SignInParam) {
-        remoteAuthDateSource.signIn(signInRequest = signInParam.toRequest())
+    override suspend fun signIn(signInParam: SignInParam) =
+        remoteAuthDateSource.signIn(signInRequest = signInParam.toRequest()).saveToken()
+
+    private fun TokenResponse.saveToken() = with(localAuthDataSource) {
+        saveAccessToken(accessToken)
+        saveRefreshToken(refreshToken)
+        saveAccessTokenExp(accessTokenExp = accessTokenExp)
+        saveRefreshTokenExp(refreshTokenExp = refreshTokenExp)
     }
 }
 
