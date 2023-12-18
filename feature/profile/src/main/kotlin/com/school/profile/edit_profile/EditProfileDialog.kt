3480c21d43646cda2cd83cac87ddf4f5555c880b
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-12-18 14:30:10 +0900

    프로필 수정 페이지 디자인 변경

--- feature/profile/src/main/kotlin/com/school/profile/edit_profile/EditProfileDialog.kt
+++ feature/profile/src/main/kotlin/com/school/profile/edit_profile/EditProfileDialog.kt
@@ -9,7 +9,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -63,6 +65,7 @@ fun EditProfileDialog(
                     .padding(vertical = 70.dp, horizontal = 16.dp)
                     .fillMaxSize()
                     .background(color = SchoolTheme.colors.white, shape = RoundedCornerShape(10.dp))
+                    .verticalScroll(state = rememberScrollState())
             ) {
                 Box(
                     modifier = Modifier
@@ -125,7 +128,7 @@ fun EditProfileDialog(
                         onNumberChange = { number = it }
                     )
                 }
-                Spacer(modifier = Modifier.weight(1F))
+                Spacer(modifier = Modifier.height(40.dp))
                 SchoolButton(text = "저장하기", horizontalPadding = 8, onClick = changeProfile)
                 Spacer(modifier = Modifier.height(34.dp))
             }
